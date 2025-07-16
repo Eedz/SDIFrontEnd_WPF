6824d74c3c65dc7710b76a59f63a52e6be7f1b4a
e3bauer <e3bauer@GFONG21A>
2025-07-16 15:47:48 -0400

    improved sublink system

--- SDIFrontEnd_WPF/MainWindowViewModel.cs
+++ SDIFrontEnd_WPF/MainWindowViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using ITC_Services;
 using ITCLib;
 using MvvmLib.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,27 +26,29 @@ namespace SDIFrontEnd_WPF
 
         public IEnumerable<MenuCategory> MenuCategories => Enum.GetValues(typeof(MenuCategory)).Cast<MenuCategory>();
 
+        public List<Survey> AvailableSurveysToAdd { get; }
+
         [ObservableProperty]
         [NotifyPropertyChangedFor(nameof(CurrentSublinks))]
         public MenuCategory selectedMenuCategory = MenuCategory.Home;
 
-        public IEnumerable<string> CurrentSublinks =>
-       SelectedMenuCategory switch
-       {
-           MenuCategory.Home => new[] { "Home" },
-               MenuCategory.Surveys => GetSurveyEditorList(),
-           MenuCategory.VarNames => new[] { "Rename", "Prefixes", "Labels" },
-           MenuCategory.Search => new[] { "Questions", "Response Sets", "Comments" },
-           MenuCategory.Praccing => new[] { "Entry", "Report", "Import", "Sheet", "Form" },
-           MenuCategory.Reports => new[] { "Single", "Comparison", "Translation", "Website", "External", "Harmony", "Products", "Parallel Vars", "Topic/Content" },
-           MenuCategory.Timing => new[] { "Survey Timing" },
-           _ => Enumerable.Empty<string>()
-       };
-
         [ObservableProperty]
-        private string selectedSublink;
+        private ObservableCollection<SublinkItem> currentSublinks;
+           
+        [ObservableProperty]
+        private SublinkItem selectedSublink;
+        [ObservableProperty]
+        private Survey selectedNewSurvey;
 
-        partial void OnSelectedSublinkChanged(string value)
+        public bool IsSurveyCategory => SelectedMenuCategory == MenuCategory.Surveys;
+
+        partial void OnSelectedMenuCategoryChanged(MenuCategory value)
+        {
+            LoadSublinks(value);
+            OnPropertyChanged(nameof(IsSurveyCategory));
+        }
+
+        partial void OnSelectedSublinkChanged(SublinkItem value)
         {
             // TODO decide if viewmodels should be disposed
             //if (ActiveForm is IDisposable disposable)
@@ -70,45 +74,142 @@ namespace SDIFrontEnd_WPF
             _surveyService = _services.GetService(typeof(ISurveyService)) as ISurveyService ?? throw new ArgumentNullException(nameof(_services), "Survey service cannot be null");
             _userService = _services.GetService(typeof(IUserService)) as IUserService ?? throw new ArgumentNullException(nameof(_services), "User service cannot be null");
             CurrentUser = _userService.GetUser(Environment.UserName) ?? throw new ArgumentNullException(nameof(_userService), "User preferences cannot be null");
+            AvailableSurveysToAdd = _surveyService.GetAllSurveys();
+            CurrentSublinks = new ObservableCollection<SublinkItem>();
         }
 
-        private async Task<ViewModelBase> OpenSurveyManager(int index)
+        private async Task<ViewModelBase> OpenSurveyManager(int surveyId)
         {
-
             // get user's filter matching the index
-            var surveyService = _services.GetService(typeof(ISurveyService)) as ISurveyService ?? throw new ArgumentNullException(nameof(_services), "Survey service cannot be null");
-            Survey survey = new ITCLib.Survey(); 
-            survey.SurveyCode = "TT1";
-            survey.Title = "Test Survey";
-            survey.Questions.Add(new SurveyQuestion("AA111", "0001"));
-            survey = await surveyService.GetSurveyByIdAsync(944);
-            var questions = surveyService.GetQuestionsForSurvey(survey.SID);
+           // int surveyId = CurrentUser.GetFilterID("frmSurveyEntry", index);
+            Survey survey = await _surveyService.GetSurveyByIdAsync(surveyId);
+            var questions = _surveyService.GetQuestionsForSurvey(surveyId);
             survey.AddQuestions(questions);
             return new SurveyManagerViewModel(_services, survey);
         }
 
         private async void SetActiveForm()
         {
-            ActiveForm = SelectedSublink switch
+            if (SelectedSublink == null)
             {
-                "Home" => new HomeViewModel(),
-                "Survey1" => await OpenSurveyManager(1),
-                "Survey2" => await OpenSurveyManager(2),
-                "Survey3" => await OpenSurveyManager(3),
-                "Questions" => new QuestionSearchViewModel(),
-                _ => null
-            };
+                ActiveForm = new HomeViewModel(); // Reset to HomeViewModel if no sublink is selected
+                return;
+            }
+
+            if (SelectedSublink is SurveySublinkItem surveySublink)
+            {
+                // If the selected sublink is a survey, open the survey manager
+                ActiveForm = await OpenSurveyManager(surveySublink.SurveyId);
+                return;
+            }else
+            {
+                ActiveForm = SelectedSublink.Key switch
+                {
+                    "home" => new HomeViewModel(),
+                    "Questions" => new QuestionSearchViewModel(),
+                    _ => null
+                };
+            }
+
+                
         }
 
-        private string[] GetSurveyEditorList()
+        private ObservableCollection<SublinkItem> GetSurveyEditorList()
         {
             var list = Enumerable.Range(1, 3)
                 .Select(i => CurrentUser.GetFilterID("frmSurveyEntry", i))
                 .ToList();
 
-            var surveyList = list.Select(x => _surveyService.GetSurveyById(x).SurveyCode).Distinct().ToArray();
+            ObservableCollection<SublinkItem> surveyList = new ObservableCollection<SublinkItem>();
+            foreach (int index in list) {
+                var survey = _surveyService.GetSurveyById(index);
+                surveyList.Add(new SurveySublinkItem(survey.SurveyCode, "survey_" + index, MenuCategory.Surveys, survey.SID));
+            }
 
             return surveyList;
+        }
+        private ObservableCollection<SublinkItem> GetVarNameLinks()
+        {
+            return new ObservableCollection<SublinkItem>()
+            {
+                new SublinkItem ("Rename Vars", "rename_vars", MenuCategory.VarNames),
+                new SublinkItem("VarName Changes", "varchanges", MenuCategory.VarNames),
+                new SublinkItem("VarName Usage", "varusage", MenuCategory.VarNames),
+                new SublinkItem("Prefix List", "prefixes", MenuCategory.VarNames),
+                new SublinkItem("VarName History", "varhistory", MenuCategory.VarNames),
+            };
+        }
+
+        private ObservableCollection<SublinkItem> GetSearchLinks()
+        {
+            return new ObservableCollection<SublinkItem>()
+            {
+                new SublinkItem("Questions", "Questions", MenuCategory.Search),
+                new SublinkItem("Response Sets", "ResponseSets", MenuCategory.Search),
+                new SublinkItem("Comments", "Comments", MenuCategory.Search),
+            };
+        }
+
+        private ObservableCollection<SublinkItem> GetPraccingLinks()
+        {
+            return new ObservableCollection<SublinkItem>()
+            {
+                new SublinkItem("Entry", "Entry", MenuCategory.Praccing),
+                new SublinkItem("Report", "Report", MenuCategory.Praccing),
+                new SublinkItem("Import", "Import", MenuCategory.Praccing),
+                new SublinkItem("Sheet", "Sheet", MenuCategory.Praccing),
+                new SublinkItem("Form", "Form", MenuCategory.Praccing),
+            };
+        }
+
+        private ObservableCollection<SublinkItem> GetReportLinks()
+        {
+            return new ObservableCollection<SublinkItem>()
+            {
+                new SublinkItem("Single", "Single", MenuCategory.Praccing),
+                new SublinkItem("Comparison", "Comparison", MenuCategory.Praccing),
+                new SublinkItem("Translation", "Translation", MenuCategory.Praccing),
+                new SublinkItem("Website", "Website", MenuCategory.Praccing),
+                new SublinkItem("External", "External", MenuCategory.Praccing),
+                new SublinkItem("Harmony", "Harmony", MenuCategory.Praccing),
+                new SublinkItem("Parallel Vars", "Parallel Vars", MenuCategory.Praccing),
+                new SublinkItem("Topic/Content", "Topic/Content", MenuCategory.Praccing),
+                
+            };
+        }
+
+        private void LoadSublinks(MenuCategory category)
+        { 
+            // This method can be used to load sublinks based on the selected category
+            // For now, it just sets the SelectedSublink to null to reset the view
+            CurrentSublinks.Clear();
+
+            IEnumerable<SublinkItem> items = category switch
+            {
+                MenuCategory.Home => new[] { new SublinkItem("Home", "home", category) },
+                MenuCategory.Surveys => GetSurveyEditorList(),
+                MenuCategory.VarNames => GetVarNameLinks(),
+                MenuCategory.Search => GetSearchLinks(),
+                MenuCategory.Praccing => GetPraccingLinks(),
+                MenuCategory.Reports => GetReportLinks(),
+                MenuCategory.Timing => new ObservableCollection<SublinkItem>() { new SublinkItem("Timing", "Timing", MenuCategory.Timing)},
+                _ => Enumerable.Empty<SublinkItem>()
+            };
+
+            foreach (var item in items)
+                CurrentSublinks.Add(item);
+        }
+
+        [RelayCommand]
+        private void AddSelectedSurvey()
+        {
+            if (SelectedNewSurvey is null || CurrentSublinks.Any(x=>x.Key.Equals("survey_" + SelectedNewSurvey.SID)))
+                return;
+
+            CurrentSublinks.Add(
+                new SurveySublinkItem(SelectedNewSurvey.SurveyCode, "survey_" + SelectedNewSurvey.SID, MenuCategory.Surveys, SelectedNewSurvey.SID  ));
+            SelectedNewSurvey = null; // reset drop down
+            
         }
     }
 }
