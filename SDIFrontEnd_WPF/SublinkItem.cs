1c3f1e52e664c79de92da421e0eec73e52cb7393
e3bauer <e3bauer@GFONG21A>
2025-07-16 15:48:02 -0400

    new class for defining links

---
+++ SDIFrontEnd_WPF/SublinkItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDIFrontEnd_WPF
+{
+    public class SublinkItem
+    {
+        public string DisplayName { get; set; }
+        public string Key { get; set; }
+        public MenuCategory Category { get; set; }
+
+        public SublinkItem(string displayName, string key, MenuCategory category)
+        {
+            DisplayName = displayName;
+            Key = key;
+            Category = category;
+        }
+
+        public override string ToString() => DisplayName;
+
+    }
+
+    public class SurveySublinkItem : SublinkItem
+    {
+        public int SurveyId { get; set; }
+        
+        public SurveySublinkItem(string displayName, string key, MenuCategory category, int surveyId)
+            : base(displayName, key, category)
+        {
+            SurveyId = surveyId;
+        
+        }
+        
+    }
+
+}
