74e28667755b94be32d703ff365bf009c12f25ea
e3bauer <e3bauer@GFONG21A>
2025-08-20 11:13:48 -0400

    Moved views Added converter for viewing HTML

---
+++ SDIFrontEnd_WPF/HtmlToFlowDocumentConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Markup;
+using System.Xml;
+
+namespace SDIFrontEnd_WPF
+{
+    public class HtmlToFlowDocumentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                string xaml = HtmlToXaml.HtmlToXamlConverter.ConvertHtmlToXaml((string)value, true);
+                FlowDocument flow = (FlowDocument) XamlReader.Parse(xaml);
+                flow.TextAlignment = System.Windows.TextAlignment.Left;
+                flow.FontFamily = new System.Windows.Media.FontFamily("Verdana");
+                flow.FontSize = 12;
+                return flow;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
