31b7cf5f587a8861f314b4fce81cc628bf3bd727
e3bauer <e3bauer@GFONG21A>
2025-08-20 11:10:35 -0400

    Added VM for editing wordings

---
+++ SDIFrontEnd_WPF/View Models/EditWordingViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DocumentFormat.OpenXml.Wordprocessing;
+using ITC_Services;
+using ITCLib;
+using Microsoft.Extensions.DependencyInjection;
+using MvvmLib.ViewModels;
+
+namespace SDIFrontEnd_WPF
+{
+    public partial class EditWordingViewModel : ViewModelBase
+    {
+        private readonly IWordingService _wordingService;
+
+        [ObservableProperty]
+        private string newWordingText;
+
+        public WordingType NewWordingType;
+
+        public List<WordingUsage> WordingUses;
+
+        partial void OnNewWordingTextChanged(string value)
+        {
+            UpdateUses(value);
+        }
+        
+
+        public EditWordingViewModel(IWordingService wordingService )
+        {
+            _wordingService = wordingService ?? throw new ArgumentNullException(nameof(wordingService));
+            WordingUses = new List<WordingUsage>();
+        }
+
+        [RelayCommand]
+        private void UpdateUses(string wordingText)
+        {
+            Wording newWording = new Wording
+            {
+                WordingText = wordingText,
+                Type = NewWordingType
+            };
+
+            WordingUses = _wordingService.GetWordingUsages(newWording);
+        }
+    }
+}
