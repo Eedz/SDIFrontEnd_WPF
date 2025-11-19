9ba204e7027c76d5d42c8ed75c129deda4b9e92d
e3bauer <e3bauer@GFONG21A>
2025-11-19 16:01:41 -0500

    Added Importer classes and interfaces

---
+++ SDIFrontEnd_WPF/Importer/IDataRowParser.cs
@@ -0,0 +1,26 @@
+﻿using ITCLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDIFrontEnd_WPF
+{
+    public interface IDataRowParser<T>
+    {
+        T Parse(IDictionary<string, string> row);
+    }
+
+    public class TranslationParser : IDataRowParser<Translation>
+    {
+        public Translation Parse(IDictionary<string, string> row)
+        {
+            return new Translation
+            {
+                VarName = row["VarName"],
+                TranslationText = row["French"]
+            };
+        }
+    }
+}
---
+++ SDIFrontEnd_WPF/Importer/IImporter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDIFrontEnd_WPF
+{
+    public interface IImporter<T>
+    {
+        IEnumerable<T> Import();
+    }
+
+    public class WordImporter<T> : IImporter<T>
+    {
+        private readonly IWordTableImporter _tableImporter;
+        private readonly IDataRowParser<T> _parser;
+
+        public WordImporter(IWordTableImporter tableImporter, IDataRowParser<T> parser)
+        {
+            _tableImporter = tableImporter;
+            _parser = parser;
+        }
+
+        public IEnumerable<T> Import()
+        {
+            _tableImporter.ReadHeadings();
+
+            foreach (var row in _tableImporter.ReadRows())
+                yield return _parser.Parse(row);
+        }
+    }
+}
