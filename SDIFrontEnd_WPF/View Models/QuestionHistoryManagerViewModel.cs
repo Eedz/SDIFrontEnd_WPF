e222b331e882bb2fb17d19262733a6f679ddba07
e3bauer <e3bauer@GFONG21A>
2026-02-23 14:42:54 -0500

    Added viewmodels and views for question history

---
+++ SDIFrontEnd_WPF/View Models/QuestionHistoryManagerViewModel.cs
@@ -0,0 +1,107 @@
+﻿using ITC_Services;
+using MvvmLib.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ITCLib;
+using CommunityToolkit.Mvvm.ComponentModel;
+using ITC_Services;
+using SDIFrontEnd_WPF.Views;
+using System.Collections.ObjectModel;
+
+    
+
+
+namespace SDIFrontEnd_WPF.ViewModels
+{
+    public partial class QuestionHistoryManagerViewModel : ViewModelBase
+    {
+        private readonly ISurveyService _surveyService;
+        private readonly IAuditService _auditService;
+        private readonly ICommentService _commentService;
+
+        public List<Survey> SurveyList { get; set; }
+
+        [ObservableProperty]
+        private Survey? selectedSurvey = null;
+
+        [ObservableProperty]
+        private VariableName? selectedVarName = null;
+
+        [ObservableProperty]
+        private Wording selectedWording = null;
+
+        [ObservableProperty]
+        private ResponseSet selectedResponse = null;
+
+        public List<VariableName> VarNameList { get; set; }
+
+        public QuestionHistoryViewModel QuestionHistoryVM { get; set; } = new QuestionHistoryViewModel() { WordingNumWidth = "0" };
+
+        public ObservableCollection<ChangedQuestionViewModel> QuestionHistoryList { get; set; } = new ObservableCollection<ChangedQuestionViewModel>();
+
+        public ObservableCollection<WordingHistoryViewModel> WordingHistoryVMs { get; set; } = new ObservableCollection<WordingHistoryViewModel>();
+        public ObservableCollection<Wording> WordingList => QuestionHistoryVM.WordingList;
+
+        public ObservableCollection<QuestionComment> Comments { get; } = new ObservableCollection<QuestionComment>();
+
+        public QuestionHistoryManagerViewModel(IAuditService auditService, ISurveyService surveyService, ICommentService commentService)
+        {
+            _auditService = auditService ?? throw new ArgumentNullException(nameof(auditService));
+            _surveyService = surveyService ?? throw new ArgumentNullException(nameof(surveyService));
+            _commentService = commentService ?? throw new ArgumentNullException(nameof(commentService));
+
+            DisplayName = "Question History Manager";
+
+            SurveyList = _auditService.GetAuditSurveys().Select(x=> new Survey(x)).ToList();
+            VarNameList = new List<VariableName>();
+        }
+
+        partial void OnSelectedSurveyChanged(Survey? oldValue, Survey newValue)
+        {
+            if (newValue == null) return;
+
+            VarNameList = _auditService.GetAuditVarNames(newValue.SurveyCode)
+                .Select(q => new VariableName { VarName = q.VarName })
+                .OrderBy(v => v.VarName)
+                .ToList();
+
+            OnPropertyChanged(nameof(VarNameList));
+        }
+
+        partial void OnSelectedVarNameChanged(VariableName? oldValue, VariableName? newValue)
+        {
+            if (newValue == null || SelectedSurvey == null) return;
+
+            var qid = _surveyService.GetQuestionID(SelectedSurvey.SurveyCode, newValue.VarName);
+            var history = _auditService.GetQuestionHistory(qid);
+            foreach (var item in history)
+            {
+                var vm = new ChangedQuestionViewModel();
+                vm.Load(item, showWordID: true);
+                QuestionHistoryList.Add(vm);
+            }
+            QuestionHistoryVM.WordingNumWidth = "0";
+            QuestionHistoryVM.Load(history);
+            OnPropertyChanged(nameof(WordingList));
+
+            Comments.Clear();
+            foreach (var c in _commentService.GetQuestionComments(qid))
+                Comments.Add(c);
+        }
+
+        partial void OnSelectedWordingChanged(Wording? oldValue, Wording newValue)
+        {
+            var history = _auditService.GetWordingHistory(newValue.FieldType, newValue.WordID);
+            WordingHistoryVMs.Clear();
+
+            foreach (var item in history)
+            {
+                WordingHistoryVMs.Add(new WordingHistoryViewModel(item));
+            }
+            OnPropertyChanged(nameof(WordingHistoryVMs));
+        }
+    }   
+}
---
+++ SDIFrontEnd_WPF/View Models/QuestionHistoryViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ITCLib;
+using MvvmLib.ViewModels;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+namespace SDIFrontEnd_WPF.ViewModels
+{
+    public partial class QuestionHistoryViewModel : ViewModelBase
+    {
+        private List<ChangedSurveyQuestion> _questionVersions;
+
+        public IEnumerable<ChangedSurveyQuestion> QuestionVersions => _questionVersions;
+
+        [ObservableProperty]
+        private ChangedSurveyQuestion? selectedVersion;
+
+        public ObservableCollection<Wording> WordingList { get; set; }
+        public ObservableCollection<ResponseSet> ResponseList { get; set; }
+
+        [ObservableProperty]
+        private Wording selectedWording;
+
+        [ObservableProperty]
+        private ResponseSet selectedResponse;
+
+        public ObservableCollection<AuditWording> WordingHistories { get; set; }
+
+        public string WordingNumWidth = "0";
+
+
+        public string CurrentQuestionText => SelectedVersion != null ? SelectedVersion.GetQuestionTextHTML() : string.Empty;
+
+        public QuestionHistoryViewModel()
+        {
+            WordingList = new ObservableCollection<Wording>();
+            ResponseList = new ObservableCollection<ResponseSet>();
+            DisplayName = "Question History";
+        }
+
+        public void Load(IEnumerable<ChangedSurveyQuestion> versions, bool showWordID = false)
+        {
+            _questionVersions = new List<ChangedSurveyQuestion>();
+            _questionVersions = versions.ToList();
+            OnPropertyChanged(nameof(QuestionVersions));
+            SelectedVersion = _questionVersions.FirstOrDefault();
+            OnPropertyChanged(nameof(CurrentQuestionText));
+            OnPropertyChanged(nameof(QuestionVersions));
+            if (showWordID) WordingNumWidth = "Auto";
+                else WordingNumWidth = "0";
+
+            OnPropertyChanged(nameof(WordingNumWidth));
+        }
+
+        partial void OnSelectedVersionChanged(ChangedSurveyQuestion? oldValue, ChangedSurveyQuestion? newValue)
+        {
+            OnPropertyChanged(nameof(CurrentQuestionText));
+
+            if (newValue == null)
+            {
+                WordingList.Clear();
+                ResponseList.Clear();
+                return;
+            }
+
+            WordingList = new ObservableCollection<Wording>
+            {
+                newValue.PrePW,
+                newValue.PreIW,
+                newValue.PreAW,
+                newValue.LitQW,
+                newValue.PstIW,
+                newValue.PstPW,
+            };
+
+            ResponseList = new ObservableCollection<ResponseSet>() { SelectedVersion.RespOptionsS, SelectedVersion.NRCodesS };
+            OnPropertyChanged(nameof(WordingList));
+        }
+
+        partial void OnSelectedResponseChanged(ResponseSet? oldValue, ResponseSet newValue)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
