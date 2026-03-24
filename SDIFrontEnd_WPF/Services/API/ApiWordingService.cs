448beb8474173ab1e94914a04de3f982a6c1e17f
e3bauer <e3bauer@GFONG21A>
2026-03-24 16:21:18 -0400

    Added methods for async access to api. Added mapping methods.

--- SDIFrontEnd_WPF/Services/API/ApiWordingService.cs
+++ SDIFrontEnd_WPF/Services/API/ApiWordingService.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using ITC_Contracts;
+using ITCLib;
+using SDIFrontEnd_WPF.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
-using ITCLib;
-using ITC_Contracts;
 
 namespace SDIFrontEnd_WPF
 {
@@ -16,44 +18,102 @@ namespace SDIFrontEnd_WPF
 
         }
 
-        Task<List<Wording>> IApiWordingService.GetAllPreP()
+        private async Task<List<WordingDto>> GetWordingsByType(string type)
         {
-            throw new NotImplementedException();
+            switch (type.ToLower())
+            {
+                case "prep": 
+                case "prei":
+                case "prea":
+                case "litq":
+                case "psti":
+                case "pstp":
+                    break;
+                default:
+                    throw new ArgumentException($"Invalid wording type: {type}");
+            }
+
+            var response = await _http.GetFromJsonAsync<List<WordingDto>>($"api/wordings/{type}");
+            return response.ToList();
         }
 
-        Task<List<Wording>> IApiWordingService.GetAllPreI()
+        private async Task<List<ResponseSetDto>> GetResponseSetsByType(string type)
         {
-            throw new NotImplementedException();
+            switch (type.ToLower())
+            {
+                case "respoptions":
+                case "nrcodes":
+                    break;
+                default:
+                    throw new ArgumentException($"Invalid wording type: {type}");
+            }
+
+            var response = await _http.GetFromJsonAsync<List<ResponseSetDto>>($"api/wordings/{type}");
+            return response.ToList();
         }
 
-        Task<List<Wording>> IApiWordingService.GetAllPreA()
+        public async Task<List<Wording>> GetAllPreP()
         {
-            throw new NotImplementedException();
+            var response = await GetWordingsByType("prep");
+
+            var wordings = response.Select(dto => MapToEntity(dto)).ToList();
+            return wordings;
         }
 
-        Task<List<Wording>> IApiWordingService.GetAllLitQ()
+        public async Task<List<Wording>> GetAllPreI()
         {
-            throw new NotImplementedException();
+            var response = await GetWordingsByType("prei");
+
+            var wordings = response.Select(dto => MapToEntity(dto)).ToList();
+            return wordings;
         }
 
-        Task<List<Wording>> IApiWordingService.GetAllPstI()
+        public async Task<List<Wording>> GetAllPreA()
         {
-            throw new NotImplementedException();
+            var response = await GetWordingsByType("prea");
+
+            var wordings = response.Select(dto => MapToEntity(dto)).ToList();
+            return wordings;
         }
 
-        Task<List<Wording>> IApiWordingService.GetAllPstP()
+        public async Task<List<Wording>> GetAllLitQ()
         {
-            throw new NotImplementedException();
+            var response = await GetWordingsByType("litq");
+
+            var wordings = response.Select(dto => MapToEntity(dto)).ToList();
+            return wordings;
         }
 
-        Task<List<ResponseSet>> IApiWordingService.GetAllRespOptions()
+        public async Task<List<Wording>> GetAllPstI()
         {
-            throw new NotImplementedException();
+            var response = await GetWordingsByType("psti");
+
+            var wordings = response.Select(dto => MapToEntity(dto)).ToList();
+            return wordings;
         }
 
-        Task<List<ResponseSet>> IApiWordingService.GetAllNonResponses()
+        public async Task<List<Wording>> GetAllPstP()
         {
-            throw new NotImplementedException();
+            var response = await GetWordingsByType("pstp");
+
+            var wordings = response.Select(dto => MapToEntity(dto)).ToList();
+            return wordings;
+        }
+
+        public async Task<List<ResponseSet>> GetAllRespOptions()
+        {
+            var response = await GetResponseSetsByType("respoptions");
+
+            var wordings = response.Select(dto => MapToEntity(dto)).ToList();
+            return wordings;
+        }
+
+        public async Task<List<ResponseSet>> GetAllNonResponses()
+        {
+            var response = await GetResponseSetsByType("nrcodes");
+
+            var wordings = response.Select(dto => MapToEntity(dto)).ToList();
+            return wordings;
         }
 
         Task<Wording> IApiWordingService.UpdateWording(Wording wording)
@@ -94,6 +154,36 @@ namespace SDIFrontEnd_WPF
         Task<int> IApiWordingService.DeleteResponseSet(ResponseSet set)
         {
             throw new NotImplementedException();
+        }
+
+        private Wording MapToEntity(WordingDto dto)
+        {
+            return new Wording
+            {
+                WordID = dto.ID,
+                Type = (WordingType)dto.Type,
+                WordingText = dto.WordingText
+            };
+        }
+
+        private ResponseSet MapToEntity(ResponseSetDto dto)
+        {
+            return new ResponseSet
+            {
+                RespSetName = dto.RespSetName,
+                Type = (ResponseType)dto.Type,
+                RespList = dto.RespList
+            };
+        }
+
+        private WordingDto MapToDto(Wording entity)
+        {
+            return new WordingDto
+            {
+                ID = entity.WordID,
+                Type = (int)entity.Type,
+                WordingText = entity.WordingText
+            };
         }
     }
 }
