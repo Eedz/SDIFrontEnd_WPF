085f83d19af78d700684ed6ed476ab25136d6e8d
e3bauer <e3bauer@GFONG21A>
2026-04-22 12:01:41 -0400

    New viewmodel for displaying question usages

---
+++ SDIFrontEnd_WPF/View Models/VarNameUsageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmLib.ViewModels;
+using ITCLib;
+namespace SDIFrontEnd_WPF.ViewModels
+{
+    public class VarNameUsageViewModel : WorkspaceViewModel
+    {
+        public List<QuestionUsage> Records { get; set; }
+
+        public VarNameUsageViewModel(List<QuestionUsage> varnames) 
+        {
+            Records = varnames;
+        }
+
+
+    }
+}
