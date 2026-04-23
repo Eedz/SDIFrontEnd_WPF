f7ce03d031a230fe84bfedffd05ec5d34e859ce5
e3bauer <e3bauer@GFONG21A>
2026-04-23 15:38:14 -0400

    New service for getting label information

---
+++ SDIFrontEnd_WPF/Services/API/ApiLabelService.cs
@@ -0,0 +1,34 @@
+﻿using ITCLib;
+using SDIFrontEnd_WPF.Mappings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using ITC_Contracts;
+using System.Net.Http.Json;
+namespace SDIFrontEnd_WPF
+{
+    public class ApiLabelService : ApiServiceBase, IApiLabelService
+    {
+        private readonly IMapperFactory _mapperFactory;
+
+        public ApiLabelService(HttpClient httpClient, IMapperFactory mapperFactory) : base(httpClient)
+        {
+            _mapperFactory = mapperFactory;
+        }
+
+        public async Task<List<VariableName>> GetVarNamesByLabel(string type, VarNameLabel label)
+        {
+            var dtos = await _http.GetFromJsonAsync<List<VariableNameDto>>($"api/labels/{type}/{label.ID}/varnames");
+
+            return dtos.Select(dto => new VariableName()
+            {
+                VarName = dto.VarName,
+                VarLabel = dto.VarLabel
+            }).ToList();
+
+        }
+    }
+}
---
+++ SDIFrontEnd_WPF/Services/API/IApiLabelService.cs
@@ -0,0 +1,14 @@
+﻿using ITCLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDIFrontEnd_WPF
+{
+    public interface IApiLabelService
+    {
+        Task<List<VariableName>> GetVarNamesByLabel(string type, VarNameLabel label);    
+    }
+}
