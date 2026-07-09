63019e4ee35f7b042db95e216c8b3690d54724d0
e3bauer <e3bauer@GFONG21A>
2026-07-09 15:36:20 -0400

    Added _baseApi to all routes so we can switch between dev and prod easier. Organized code with regions. Added create and update methods for translations.

--- SDIFrontEnd_WPF/Services/API/ApiQuestionService.cs
+++ SDIFrontEnd_WPF/Services/API/ApiQuestionService.cs
@@ -15,15 +15,18 @@ namespace SDIFrontEnd_WPF
     public class ApiQuestionService : ApiServiceBase, IApiQuestionService
     {
         private readonly IMapper<SurveyQuestion, SurveyQuestionDto> _surveyQuestionMapper;
+        private readonly IMapper<Translation, TranslationDto> _translationMapper;   
 
         public ApiQuestionService(HttpClient http, IMapperFactory mapperFactory) : base(http)
         {
             _surveyQuestionMapper = mapperFactory.Get<SurveyQuestion, SurveyQuestionDto>();
         }
 
+
+        #region GET Methods
         public async Task<SurveyQuestion?> GetQuestionByIdAsync(int id)
         {
-            var dto = await _http.GetFromJsonAsync<SurveyQuestionDto>($"api/questions/{id}");
+            var dto = await _http.GetFromJsonAsync<SurveyQuestionDto>($"{_baseApi}/questions/{id}");
 
             var question = _surveyQuestionMapper.MapToEntity(dto);
             return question;
@@ -31,7 +34,7 @@ namespace SDIFrontEnd_WPF
 
         public async Task<List<SurveyQuestion>> GetQuestionsByVarNameAsync(string varname)
         {
-            var dtos = await _http.GetFromJsonAsync<IEnumerable<SurveyQuestionDto>>($"api/questions/by-var/{varname}");
+            var dtos = await _http.GetFromJsonAsync<IEnumerable<SurveyQuestionDto>>($"{_baseApi}/questions/by-var/{varname}");
 
             var question = dtos.Select(x => _surveyQuestionMapper.MapToEntity(x));
             return question.ToList();
@@ -39,71 +42,82 @@ namespace SDIFrontEnd_WPF
 
         public async Task<List<SurveyQuestion>> GetQuestionsByRefVarNameAsync(string varname)
         {
-            var dtos = await _http.GetFromJsonAsync<IEnumerable<SurveyQuestionDto>>($"api/questions/by-ref/{varname}");
+            var dtos = await _http.GetFromJsonAsync<IEnumerable<SurveyQuestionDto>>($"{_baseApi}/questions/by-ref/{varname}");
 
             var question = dtos.Select(x => _surveyQuestionMapper.MapToEntity(x));
             return question.ToList();
         }
 
-        public async Task<SurveyQuestionRecord> SaveQuestion(SurveyQuestionRecord question)
-        {
-            var dto = _surveyQuestionMapper.MapToDto(question.Item);
-
-            HttpResponseMessage response;
-            if (question.NewRecord)
-                response = await _http.PostAsJsonAsync($"api/questions", dto);
-            else if (question.Deleted)
-                response = await _http.DeleteAsync($"api/questions/{dto.ID}");
-            else if (question.ShouldSave)
-            {
-                if (question.DirtyLabels)
-                    await _http.PutAsJsonAsync($"api/labels/variable/{dto.VarName.VarName}", dto.VarName);
-                response = await _http.PutAsJsonAsync($"api/questions/{dto.ID}", dto);
-            }
-            else
-                return question; // No changes to save
-
-            response.EnsureSuccessStatusCode();
-            return question;
-        }
-
         public async Task<List<SurveyQuestion>> SearchQuestions(string searchTerm)
         {
-            var dtos = await _http.GetFromJsonAsync<IEnumerable<SurveyQuestionDto>>($"api/questions/search?query={Uri.EscapeDataString(searchTerm)}");           
+            var dtos = await _http.GetFromJsonAsync<IEnumerable<SurveyQuestionDto>>($"{_baseApi}/questions/search?query={Uri.EscapeDataString(searchTerm)}");
             var questions = dtos.Select(x => _surveyQuestionMapper.MapToEntity(x));
             return questions.ToList();
         }
+        #endregion
+
+
+       
+
+        #region POST Methods
 
         public async Task<int> AddQuestion(SurveyQuestion question)
         {
             var dto = _surveyQuestionMapper.MapToDto(question);
-            var response = await _http.PostAsJsonAsync($"api/questions", dto);
+            var response = await _http.PostAsJsonAsync($"{_baseApi}/questions", dto);
             response.EnsureSuccessStatusCode();
             var createdDto = await response.Content.ReadFromJsonAsync<SurveyQuestionDto>();
             return createdDto.ID;
         }
 
+        public async Task<bool> CreateTranslation(Translation translation)
+        {
+            var dto = _translationMapper.MapToDto(translation);
+            var response = await _http.PostAsJsonAsync($"{_baseApi}/questions/{translation.QID}/translations", dto);
+            response.EnsureSuccessStatusCode();
+            return response.IsSuccessStatusCode;
+        }
+        #endregion
+
+        #region PUT Methods
         public async Task<int> UpdateQuestion(SurveyQuestion question)
         {
             var dto = _surveyQuestionMapper.MapToDto(question);
-            var response = await _http.PutAsJsonAsync($"api/questions/{dto.ID}", dto);
+            var response = await _http.PutAsJsonAsync($"{_baseApi}/questions/{dto.ID}", dto);
             response.EnsureSuccessStatusCode();
             return dto.ID;
         }
+        public async Task<bool> UpdateTranslation(Translation translation)
+        {
+            var dto = _translationMapper.MapToDto(translation);
+            var response = await _http.PutAsJsonAsync($"{_baseApi}/questions/{translation.QID}/translations/{translation.ID}", dto);
+            response.EnsureSuccessStatusCode();
+            return response.IsSuccessStatusCode;
+        }
 
+        #endregion
+
+        #region PATCH
         public async Task<bool> UpdateQnums(IEnumerable<SurveyQuestion> questions)
         {
-            var dtos = questions.Select(x=>_surveyQuestionMapper.MapToDto(x));
-            var response = await _http.PatchAsJsonAsync($"api/questions/question-order", dtos);
+            var dtos = questions.Select(x=>_surveyQuestionMapper.MapToDto(x)).ToList();
+            var response = await _http.PatchAsJsonAsync($"{_baseApi}/questions/question-order", dtos);
             var result = response.EnsureSuccessStatusCode();
             return result.IsSuccessStatusCode;
         }
 
+        #endregion
+
+        #region DELETE Methods
+
         public async Task<bool> DeleteQuestion(SurveyQuestion question)
         {
-            var response = await _http.DeleteAsync($"api/questions/{question.ID}");
+            var response = await _http.DeleteAsync($"{_baseApi}/questions/{question.ID}");
             var result = response.EnsureSuccessStatusCode();
             return result.IsSuccessStatusCode;
-        }       
+        }
+        #endregion 
+      
+        
     }
 }
